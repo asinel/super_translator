6843302bec85ba8fce31ec03c873b395a3d48ddf
Viktor Sinelnikov <sinelnikovv97@gmail.com>
2021-03-20 13:09:06 +0100

    Tab navigation

---
+++ lib/presentation/main_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:super_translator/presentation/favorites_screen.dart';
+import 'package:super_translator/presentation/translator_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Super Translator'),
+          ),
+          bottomNavigationBar: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.translate), text: 'Translate',),
+              Tab(icon: Icon(Icons.favorite), text: 'Favorite',)
+            ],
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.grey,
+          ),
+          body: TabBarView(
+            children: [
+              TranslatorScreen(),
+              FavoritesScreen()
+            ],
+          ),
+        )
+    );
+  }
+}
